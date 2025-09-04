cbd5d7ee09256d6b695b1fd96e38389ab979f13d
himanshu aaithan <himanshukumawat9649@gmail.com>
2025-09-04 08:31:22 +0530

    menubtn update

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 let video = document.querySelector("#videohover")
+let menubtn = document.querySelector(".menu")
+let menubar = document.querySelector(".menuprt")
+let main = document.querySelector(".main")
+let cross = document.querySelector(".cross")
 
-
-video.addEventListener("mouseenter", function(){
+video.addEventListener("mouseenter", function () {
     video.play();
 })
+
+menubtn.addEventListener("click", function () {
+
+    main.style.display = "none"
+    menubar.style.display = "inherit"
+})
+
+cross.addEventListener("click", function () {
+    main.style.display = "inherit"
+    menubar.style.display = "none"
+})
+
+
 
 
